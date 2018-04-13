1932677f3df4c25c5ba7815cd9bb7b7d277cdae0
Edgar Giovanni Lepe <lepe.edgar10@gmail.com>
2018-04-13 14:42:08 -0700

    Added module description

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,10 @@
+{-|
+Module      : Main
+Description : This module contains the application
+Copyright   : (c) Edgar Giovanni Lepe, 2018
+License     : BSD3
+Portability :
+-}
 module Main where
 
 import Lib
